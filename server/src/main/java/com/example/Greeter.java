cc9ffc5c5e1cb38a5b0c9d2c0a2fbb6ea60805b5
Silvano Gil <sgil@pronoide.es>
2020-02-13 11:06:20 +0100

    Cambios para fallo del PMD + Checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,8 +17,15 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    //Long lalala = new Long("2333");
+    Long lalala = new Long("2333");
     if(magico > 1){
+      if(true){
+         if(true){
+         if(true){
+        System.out.println("Hola caracola");
+      }
+      }
+      }
         return String.format("Hello, %s!", someone);
     }
      else{
