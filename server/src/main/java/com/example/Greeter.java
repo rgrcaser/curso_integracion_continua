1867fc556ddb7ad9ec364490e98bdb4a6d20ad00
Silvano Gil <sgil@pronoide.es>
2020-02-19 13:17:25 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,6 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    Long lalala = new Long("2333");
     if(magico > 1){
       if(true){
          if(true){
